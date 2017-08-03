d46b1f42cac69b00975b9b268284dcafcc1a8390
Edgar Sandoval Walle <hsandovalwalle@apple.com>
2017-08-03 14:05:09 -0700

    Changed output template

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ function InsertAtBodyEnd(options) {
 
   if (!this.options.scriptSrc) throw new Error('html-webpack-insert-at-body-end-plugin requires a scriptSrc property to be set in the options');
 }
- 
+
 InsertAtBodyEnd.prototype.apply = function(compiler) {
   const that = this;
-  compiler.plugin('compilation', function(compilation) { 
+  compiler.plugin('compilation', function(compilation) {
     compilation.plugin('html-webpack-plugin-after-html-processing', function(htmlPluginData, callback) {
       if (htmlPluginData.plugin.options.filename !== that.options.filename) return callback(null, htmlPluginData);
 
-      const scriptTag = '  <script src="' + that.options.scriptSrc + '" type="text/javascript"></script>\n'
+      const scriptTag = '  <script type="text/javascript">' + that.options.scriptSrc + '</script>\n'
       htmlPluginData.html = htmlPluginData.html.replace(CLOSE_BODY_TAG, scriptTag + CLOSE_BODY_TAG);
 
       callback(null, htmlPluginData);
     });
   });
 };
- 
+
 module.exports = InsertAtBodyEnd;
